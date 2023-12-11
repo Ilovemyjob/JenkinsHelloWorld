9d02ab39a85d4b0594f35663607570c732730370
emrahalpaslan <64224084+Ilovemyjob@users.noreply.github.com>
2023-12-11 11:21:02 +0100

    Update deneme.java

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -1,9 +1,7 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
 public class deneme {
 
     public static void main(String[] args) {
-        System.out.println("deneme");
+        System.out.println("ben calisiyorum");
     }
 
 
