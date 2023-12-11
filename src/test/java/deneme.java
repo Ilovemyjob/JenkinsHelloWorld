e936e6a825fe93a8e9203834af61beffab94a4e0
emrahalpaslan <64224084+Ilovemyjob@users.noreply.github.com>
2023-12-11 13:31:54 +0100

    Update deneme.java

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -5,6 +5,7 @@ public class deneme {
         System.out.println("Ben calisiyorum");
 	System.out.println("Ben 2. kere calisiyorum");
 	System.out.println("Ben 3. kere calisiyorum");
+	    System.out.println("Ben 4. kere calisiyorum");
     }
 
 
