862819f1374e655731fde815eb1310ba9ce4774c
Emrah Alpaslan <emrah.alpaslan@expleogroup.com>
2023-12-11 13:18:39 +0100

    geliyor 3

--- src/test/java/deneme.java
+++ src/test/java/deneme.java
@@ -4,6 +4,7 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("Ben calisiyorum");
 	System.out.println("Ben 2. kere calisiyorum");
+	System.out.println("Ben 3. kere calisiyorum");
     }
 
 
